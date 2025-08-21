7bec05957509e1f9a9885dc7d323b62a6c5797fc
Mustafa Chaudhry <65905290+MustafaCh1@users.noreply.github.com>
2025-08-21 15:56:48 +0100

    Update autotraderScraper.py

--- autotraderScraper.py
+++ autotraderScraper.py
@@ -14,8 +14,8 @@ import pandas as pd
 from sqlalchemy import desc
 from sympy import pprint
 import re
-import google.generativeai as genai
-from google.generativeai import types
+from google import genai
+from google.genai import types
 import base64
 from sentence_transformers import SentenceTransformer, util
 from rapidfuzz import fuzz, process
